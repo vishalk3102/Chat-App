fcc97a0b42775b3e29ee8b6498c71af9d4d9b8f2
vishal kumar <vishal.k3102@gmail.com>
2024-06-27 11:33:57 +0530

    maded changes in reset and forget password page

--- forgetPass.php
+++ forgetPass.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password recovery</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
+<style>
+    .button {
+        margin-bottom: 2px !important;
+    }
+
+    .back-to-login a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 5px 0px;
+        text-decoration: none;
+        color: #000;
+
+    }
+
+    .back-to-login a span {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .back-to-login a span img {
+        height: 20px;
+        width: 30px;
+        padding: 4px;
+        margin-right: 4px;
+    }
+
+    .back-to-login a p {
+        font-size: 12px;
+        font-weight: 600;
+    }
+</style>
+
 <body>
     <div class="container log-container">
         <div class="title">Password recovery</div>
@@ -18,10 +54,17 @@
                     </div>
                 </div>
                 <div class="button">
-                    <input type="submit" value="Submit">
+                    <input type="submit" value="Reset Password">
+                </div>
+                <div class="back-to-login">
+                    <a href="index.php">
+                        <span><img src="./assets/arrow.png" alt=""></span>
+                        <p>Back to Log in</p>
+                    </a>
                 </div>
             </form>
         </div>
     </div>
 </body>
+
 </html>
--- resetPassword.php
+++ resetPassword.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password recovery</title>
     <link rel="stylesheet" href="style/style.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script>
         function validateForm() {
-           
+
             var email = document.getElementById('email').value.trim();
             var password = document.getElementById('password').value;
             var confirmPassword = document.getElementById('confirmPassword').value;
@@ -21,7 +22,7 @@
             }
 
             // Validate password and confirm password match
-            if(password.length<6){
+            if (password.length < 6) {
                 alert("Password length should be at least 6");
                 return false;
             }
@@ -33,19 +34,48 @@
             return true;
         }
     </script>
-
 </head>
+<style>
+    .button {
+        margin-bottom: 2px !important;
+    }
+
+    .back-to-login a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 5px 0px;
+        text-decoration: none;
+        color: #000;
+
+    }
+
+    .back-to-login a span {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .back-to-login a span img {
+        height: 20px;
+        width: 30px;
+        padding: 4px;
+        margin-right: 4px;
+    }
+
+    .back-to-login a p {
+        font-size: 12px;
+        font-weight: 600;
+    }
+</style>
+
 <body>
-<div class="container log-container">
+    <div class="container log-container">
         <div class="title">Password recovery</div>
         <div class="content">
             <form action="#" onsubmit="return validateForm()">
                 <div class="user-details fileds">
-                   
-                    <div class="input-box">
-                        <span class="details">Email</span>
-                        <input type="email" id="email" placeholder="Enter your email" required>
-                    </div>
                     <div class="input-box">
                         <span class="details">New Password</span>
                         <input type="password" id="password" placeholder="Enter your password" required>
@@ -62,9 +92,15 @@
                 <div class="button">
                     <input type="submit" value="Submit">
                 </div>
-                
+                <div class="back-to-login">
+                    <a href="index.php">
+                        <span><img src="./assets/arrow.png" alt=""></span>
+                        <p>Back to Log in</p>
+                    </a>
+                </div>
             </form>
         </div>
     </div>
 </body>
+
 </html>
